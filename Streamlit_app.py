a6bc8644201294d9396a8ec2aa652270887950a4
Mathana Mathav <62739618+mathanamathav@users.noreply.github.com>
2023-03-02 23:24:02 +0530

    Create Streamlit_app.py

---
+++ Streamlit_app.py
@@ -0,0 +1,18 @@
+# Libraries
+import streamlit as st
+
+st.write(
+    """
+    The crypto industry continues to progress and its development has never stopped. Contributors
+    of each blockchain keep developing each segment of the industry and the whole crypto ecosystem.
+    This tool is designed to allow viewers to journey into the world of crypto ecosystems of some
+    of the major blockchains, and compare their performance.
+    This tool is designed and structured in multiple **Pages** that are accessible using the sidebar.
+    Each of these Pages addresses a different segment of the crypto industry. Within each segment
+    (Macro, Transfers, Swaps, NFTs, etc.) you are able to filter your desired blockchains to
+    narrow/expand the comparison. By selecting a single blockchain, you can observe a deep dive
+    into that particular network.
+    All values for amounts, prices, and volumes are in **U.S. dollars** and the time frequency of the
+    analysis was limited to the last **30 days**.
+    """
+)
